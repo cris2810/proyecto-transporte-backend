817d7d88a86082da8b9d5d5730a3bb35fa7c6526
cris2810 <c.olguinorellana1@uandresbello.edu>
2025-11-19 19:46:55 -0300

    dddffssaa

--- server.js
+++ server.js
@@ -6,32 +6,38 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const paymentRoutes = require('./routes/paymentRoutes.js');
 const pagoRoutes = require('./routes/pagoRoutes.js');
+const  registerUser  = require('./controllers/userController.js');
 const uploadRoutes = require('./routes/uploadRoutes.js');
-const asistenciaRoutes = require('./routes/asistenciaRoutes.js'); // 👈 OK
-
+const Asistencia = require('./models/Asistencia.js');
+const asistenciaRoutes = require('./routes/asistenciaRoutes.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware para parsear JSON
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
+
+
+// Conectar a MongoDB
 mongoose.connect('mongodb+srv://cristianolguin22:slNEUCUgxPdHS7sL@cluster1.bpl97o3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1', {
  
 })
 .then(() => console.log('Conectado a MongoDB'))
 .catch((error) => console.log('Error de conexión a MongoDB:', error));
 
-
-// Rutas API
+// Usar rutas
 app.use('/api', userRoutes);
 app.use('/api/auth', loginRoutes);
 app.use('/api', paymentRoutes);
 app.use('/api', pagoRoutes);
-app.use('/api', uploadRoutes);
+app.use("/api", uploadRoutes);
+app.use("/api", asistenciaRoutes);
 
-// 👇 MONTA ASISTENCIA EN /api/asistencia
-app.use('/api/asistencia', asistenciaRoutes);
+
+// Ruta de autenticaciónq
+
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
