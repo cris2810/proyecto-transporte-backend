71428326b43e28ff58f08377a968f03350576575
cris2810 <c.olguinorellana1@uandresbello.edu>
2025-11-19 20:45:42 -0300

    ddeerree

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const cors = require('cors');
 const paymentRoutes = require('./routes/paymentRoutes.js');
 const pagoRoutes = require('./routes/pagoRoutes.js');
 const  registerUser  = require('./controllers/userController.js');
-const Asistencia = require('./models/Asistencia.js');
 const asistenciaRoutes = require('./routes/asistenciaRoutes.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
